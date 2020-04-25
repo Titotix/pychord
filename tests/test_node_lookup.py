6dc5992d53ac288dcb62184a5ea1525089aade98
Thibault Marquand <titotix@free.fr>
2020-04-25 12:11:32 +0200

    reintroduce test_threenodesring()

--- tests/test_node_lookup.py
+++ tests/test_node_lookup.py
@@ -84,17 +84,18 @@ class NodeaddToRingTest(unittest.TestCase):
         self.assertEqual(self.node1.successor.uid.value, self.node0.uid.value)
         self.assertEqual(self.node2.successor.uid.value, self.node1.uid.value)
 
-        #node0 a2fa69d06e3fdca9e022f993e81081d3cc65b262d4a77bd47caa190b7180d354
-        #node1 4e03fd4d8bfdf5b10d1d6bbda1bc91caffb6be6740e3baade0851c3b57b3a140
-        #node2 e40ba7ecf4e34deb177ed2bcca881338e8b98502e3f3ebab4a122b75bd6ba18f
-#    def test_ThreeNodesRing(self):
-#        """
-#        Test addToRing methods with a ring of 3 nodes
-#        node1 added by node0, node 2 added by node1 (aka no 'main node')
-#        Check if successor attr is the proper one
-#        """
-#        self.node0.addToRing(self.node1)
-#        self.node1.addToRing(self.node2)
-#        self.assertTrue(self.node0.successor is self.node1)
-#        self.assertTrue(self.node1.successor is self.node2)
-#        self.assertTrue(self.node2.successor is self.node0)
+    def test_ThreeNodesRing(self):
+        """
+        Test addToRing methods with a ring of 3 nodes
+        Two nodes added by two nodes
+        It provides a dict for addToRing method
+        Check if successor attr is the proper one
+        """
+        # add node1 from node0
+        self.node0.addToRing({"ip": self.node1.ip, "port": self.node1.port})
+        # add node2 from node1
+        self.node1.addToRing({"ip": self.node2.ip, "port": self.node2.port})
+
+        self.assertEqual(self.node0.successor.uid.value, self.node2.uid.value)
+        self.assertEqual(self.node1.successor.uid.value, self.node0.uid.value)
+        self.assertEqual(self.node2.successor.uid.value, self.node1.uid.value)
